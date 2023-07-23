a8210f4aa44d5ac4e3b1fd2c85a1bc8da1cb737b
fgz21 <flowhister30@gmail.com>
2023-07-23 14:14:21 -0400

    nueva modifacacion 23/07/2023

--- backend/sql/user.sql
+++ backend/sql/user.sql
@@ -3,5 +3,6 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   uuid VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  rol ENUM('usuario', 'admin') NOT NULL DEFAULT 'usuario',
 );
