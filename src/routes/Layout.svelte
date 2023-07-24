7cd076e403dfe0b8f161e84a3e688f3c78ca1189
Erick Ramirez <erickgiber7@gmail.com>
2023-07-23 23:14:48 -0400

    Removing footer in dashboard and profile

--- src/routes/Layout.svelte
+++ src/routes/Layout.svelte
@@ -1,7 +1,20 @@
 <script>
+  // @ts-nocheck
+  import { useLocation } from "svelte-routing";
   import { FlatToast, ToastContainer } from "svelte-toasts";
   import Footer from "../lib/components/Footer.svelte";
   import Header from "../lib/components/Header.svelte";
+
+  let location = useLocation();
+  let path = $location.pathname;
+  let disableFooter;
+
+  $: disableFooter = () => {
+    path = $location.pathname;
+    if (path === "/" || path === "/profile") {
+      return true;
+    }
+  };
 </script>
 
 <ToastContainer placement="bottom-right" let:data>
@@ -10,5 +23,7 @@
 <main class="min-h-screen bg-gray-900 text-white">
   <Header />
   <slot />
-  <Footer />
+  {#if !disableFooter()}
+    <Footer />
+  {/if}
 </main>
--- src/routes/Private/Profile.svelte
+++ src/routes/Private/Profile.svelte
@@ -1,23 +1,23 @@
 <script>
   import Icon from "@iconify/svelte";
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   let user = {
-    name: '',
-    email: '',
-    cedula: '',
-    gender: '',
-    career: '',
-    semestre: ''
+    name: "",
+    email: "",
+    cedula: "",
+    gender: "",
+    career: "",
+    semestre: "",
   };
 
   function saveData() {
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem("user", JSON.stringify(user));
     showSavedProfile();
   }
 
   function showSavedProfile() {
-    const savedUser = JSON.parse(localStorage.getItem('user'));
+    const savedUser = JSON.parse(localStorage.getItem("user"));
     if (savedUser) {
       user = savedUser;
     }
@@ -25,15 +25,19 @@
 
   // Obtener los datos del almacenamiento local al cargar el componente
   onMount(() => {
-    const savedUser = JSON.parse(localStorage.getItem('user'));
+    const savedUser = JSON.parse(localStorage.getItem("user"));
     if (savedUser) {
       user = savedUser;
     }
   });
 </script>
 
-<main class="w-full min-h-screen bg-gray-900 flex items-center justify-center text-[#0ea5e9] pt-6 bt-8">
-  <div class="bg-gray-900 shadow-md px-8 pt-6 pb-8 mb-4 flex flex-col w-full sm:w-2/3 lg:w-1/2">
+<main
+  class="w-full min-h-screen bg-gray-900 flex items-center justify-center text-[#0ea5e9] pt-6 bt-8"
+>
+  <div
+    class="bg-gray-900 shadow-md px-8 pt-6 pb-8 mb-4 flex flex-col w-full sm:w-2/3 lg:w-1/2"
+  >
     <h1 class="text-2xl flex items-center gap-1 mb-5">
       <Icon icon="ph:user" />
       Mi perfil
@@ -44,29 +48,48 @@
           <Icon icon="icon-park-outline:edit-name" />
           Nombre
         </label>
-        <input class="form-input rounded-md shadow-sm w-full sm:text-sm sm:leading-5" type="text" id="name" bind:value={user.name}/>
+        <input
+          class="form-input rounded-md shadow-sm w-full sm:text-sm sm:leading-5"
+          type="text"
+          id="name"
+          bind:value={user.name}
+        />
       </div>
       <div class="w-full sm:w-1/2">
-        <label class="flex  items-center gap-1" for="email">
+        <label class="flex items-center gap-1" for="email">
           <Icon icon="ic:outline-email" />
           Correo electrónico
         </label>
-        <input class="form-input border border-[#0ea5e9]  rounded-md shadow-sm w-full sm:text-sm sm:leading-5" type="email" id="email" bind:value={user.email}/>
+        <input
+          class="form-input border border-[#0ea5e9] rounded-md shadow-sm w-full sm:text-sm sm:leading-5"
+          type="email"
+          id="email"
+          bind:value={user.email}
+        />
       </div>
       <div class="w-full sm:w-1/2">
         <label class="flex items-center gap-1" for="cedula">
           <Icon icon="ant-design:user-outlined" />
           Número de cédula
         </label>
-        <input class="form-input rounded-md border border-[#0ea5e9]  shadow-sm w-full sm:text-sm sm:leading-5" type="text" id="cedula" bind:value={user.cedula}/>
+        <input
+          class="form-input rounded-md border border-[#0ea5e9] shadow-sm w-full sm:text-sm sm:leading-5"
+          type="text"
+          id="cedula"
+          bind:value={user.cedula}
+        />
       </div>
       <div class="w-full sm:w-1/2">
         <label class="flex items-center gap-1" for="gender">
-          <Icon icon="mdi:gender-male" class="w-5 h-5"/>
-          <Icon icon="mdi:gender-female" class="w-5 h-5"/>
+          <Icon icon="mdi:gender-male" class="w-5 h-5" />
+          <Icon icon="mdi:gender-female" class="w-5 h-5" />
           Género
         </label>
-        <select class="form-select rounded-md border border-[#0ea5e9]  shadow-sm w-full sm:text-sm sm:leading-5" id="gender" value={user.gender} on:change={e => user.gender = e.target.value}>
+        <select
+          class="form-select rounded-md border border-[#0ea5e9] shadow-sm w-full sm:text-sm sm:leading-5"
+          id="gender"
+          bind:value={user.gender}
+        >
           <option value="">Selecciona un género</option>
           <option value="masculino">Masculino</option>
           <option value="femenino">Femenino</option>
@@ -78,14 +101,23 @@
           <Icon icon="bi:file-earmark-person" />
           Carrera
         </label>
-        <input class="form-input rounded-md border border-[#0ea5e9]  shadow-sm w-full sm:text-sm sm:leading-5" type="text" id="career" bind:value={user.career}/>
+        <input
+          class="form-input rounded-md border border-[#0ea5e9] shadow-sm w-full sm:text-sm sm:leading-5"
+          type="text"
+          id="career"
+          bind:value={user.career}
+        />
       </div>
       <div class="w-full sm:w-1/2">
         <label class="flex items-center gap-1" for="semestre">
           <Icon icon="bx:bxs-calendar-star" />
           Semestre
         </label>
-        <select class="form-select border border-[#05e9] rounded-md shadow-sm w-full sm:text-sm sm:leading-5" id="semestre" value={user.semestre} on:change={e => user.semestre = e.target.value}>
+        <select
+          class="form-select border border-[#05e9] rounded-md shadow-sm w-full sm:text-sm sm:leading-5"
+          id="semestre"
+          bind:value={user.semestre}
+        >
           <option value="">Selecciona un semestre</option>
           <option value="1">1</option>
           <option value="2">2</option>
@@ -96,11 +128,13 @@
           <option value="7">7</option>
           <option value="8">8</option>
           <option value="9">9</option>
-          
         </select>
       </div>
       <div class="w-full flex items-center justify-start mt-6">
-        <button class="bg-[#0ea5e9] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+        <button
+          class="bg-[#0ea5e9] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
           Guardar cambios
         </button>
       </div>
